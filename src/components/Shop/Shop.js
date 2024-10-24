b36cca0af95db23492cfae6abf00119e7183a5fe
mdalamin123a <md.alamindev90@gmail.com>
2024-10-24 22:42:46 +0600

    34-12-Cart-State-Setup-And-Update-Cart-State

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,13 +7,17 @@ const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
 
+    const [cart, setCart] = useState([]);
+
     const handleAddProduct = (product) => {
-        console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
     } 
 
     return (
         <div className="shop-container">
             <div className="product-container">
+
             {
                 products.map(pd => <Product
                     handleAddProduct={handleAddProduct} 
@@ -22,6 +26,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h3>This is cart</h3>
+                <h5>Ordar Summary: {cart.length}</h5>
             </div>
         </div>
     );
