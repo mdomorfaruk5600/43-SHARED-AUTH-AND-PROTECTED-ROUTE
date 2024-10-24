c91734a8900541550c070c982eae9b2f1885e3b8
mdalamin123a <md.alamindev90@gmail.com>
2024-10-24 22:31:16 +0600

    34-11-Pass-Event-Handler-Child-Component-And-Handle-Event

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,11 +7,17 @@ const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
 
+    const handleAddProduct = (product) => {
+        console.log(product);
+    } 
+
     return (
         <div className="shop-container">
             <div className="product-container">
             {
-                products.map(pd => <Product product={pd}></Product>)
+                products.map(pd => <Product
+                    handleAddProduct={handleAddProduct} 
+                    product={pd}></Product>)
             }
             </div>
             <div className="cart-container">
