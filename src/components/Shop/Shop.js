74a724798f1bf55593af5e4e0a740cf5e51d4699
mdalamin123a <md.alamindev90@gmail.com>
2024-10-24 12:09:04 +0600

    34-7-Component-Layout-Use-FlexBox-Setup-Area-Container

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,15 +5,19 @@ import fakeData from '../../fakeData/products';
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
+
     return (
-        <div>
-            <h1>this is shop</h1>
-            <h3>{products.length}</h3>
+        <div className="shop-container">
+            <div className="product-container">
             <ul>
                 {
                     products.map(product => <li>{product.name}</li>)
                 }
             </ul>
+            </div>
+            <div className="cart-container">
+                <h3>This is cart</h3>
+            </div>
         </div>
     );
 };
