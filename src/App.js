5adc095ce9c03743c43536b5da4d4f81dd5a327a
mdalamin123a <md.alamindev90@gmail.com>
2024-10-30 08:09:45 +0600

    37-3-Setup-Router-Router-Path-exact-path-not-fund-path

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Routes>
+          <Route path='shop' element={<Shop></Shop>}/>
+          <Route path='review' element={<Review></Review>} />
+          <Route path='inventory' element={<Inventory></Inventory>} />
+          <Route exact path='/' element={<Shop></Shop>} />
+          <Route  path='*' element={<NotFound></NotFound>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
