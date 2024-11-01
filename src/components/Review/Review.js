ce73bca2e086897d7e48ef592b3d305d15db8f39
mdalamin123a <md.alamindev90@gmail.com>
2024-11-01 23:12:40 +0600

    38-2-Remove-From-Cart-Pass-Parameter-On-Click-Remove-From-State

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import {getDatabaseCart} from '../../utilities/fakedb';
+import {getDatabaseCart, removeFromDatabaseCart} from '../../utilities/fakedb';
 import products from '../../fakeData/products'
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
     const [cart, setCart] = useState([]);
 
     useEffect(()=>{
         const savedCart = getDatabaseCart();
+        console.log(savedCart);
         const productKeys = Object.keys(savedCart);
         const cartProducts = productKeys.map(key => {
             const product = products.find(pd => pd.key === key);
@@ -15,11 +17,19 @@ const Review = () => {
         });
         setCart(cartProducts);
     }, []);
-    console.log(cart);
+
+    const removeProduct = (productKey) => {
+        const newCart = cart.filter(pd => pd.key !== productKey);        
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
 
     return (
         <div>
             <h1>Cart Items : {cart.length}</h1>
+            {
+                cart.map(pd => <ReviewItem key={pd.key} removeProduct={removeProduct} product={pd}></ReviewItem>)
+            }
         </div>
     );
 };
--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -17,6 +17,7 @@ const getDataKey = () => {
 // push to local storage: a temporary place for database
 const getDatabaseCart = () => {
   const dataKey = getDataKey();
+
   const data = localStorage.getItem(dataKey) || "{}";
   return JSON.parse(data);
 }
