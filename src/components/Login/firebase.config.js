166148e4a328aed0d697aae7418f898256a3324a
mdomorfaruk5600 <md.omorfaruk5600@gmail.com>
2025-01-25 09:11:09 +0600

    Update firebase.config.js

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,11 +1,4 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAOmyMWVbe6QZqCtLtuPg9p5tFNlFzyAUQ",
-    authDomain: "ema-jhon-simple-5a85d.firebaseapp.com",
-    projectId: "ema-jhon-simple-5a85d",
-    storageBucket: "ema-jhon-simple-5a85d.firebasestorage.app",
-    messagingSenderId: "439205596521",
-    appId: "1:439205596521:web:0971fd6598905596d8c1f8",
-    measurementId: "G-PR43N35JVC"
   };
   
   export default firebaseConfig;
